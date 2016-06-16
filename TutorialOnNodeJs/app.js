b867b733484af6b2af2d95078d3c4fdf15d47cc4
mou3ez <latrach.moez@gmail.com>
2016-06-16 03:09:25 +0100

    inclure les fichier angular et bower component

--- TutorialOnNodeJs/app.js
+++ TutorialOnNodeJs/app.js
@@ -23,6 +23,8 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/bower_components' , express.static(__dirname + '/bower_components'));
+
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
