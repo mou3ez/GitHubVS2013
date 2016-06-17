c26dd8e1276d903d034e43789b3cf18e62a34d20
moez latrach <latrach.moez@gmail.com>
2016-06-17 14:22:59 +0100

    message de validation pour le formulaire create category en cours de developpement ( video 5 )

--- TutorialOnNodeJs/public/javascripts/app/productCategory/productCategoryController.js
+++ TutorialOnNodeJs/public/javascripts/app/productCategory/productCategoryController.js
@@ -13,18 +13,81 @@ function productCategoryController($scope, $timeout , productCategoryService) {
 
     };
 
+    $scope.message = {
+        
+        containsSuccessfulMessage : false,
+        successfulMessage : ""
+    };
+
+
+    $scope.validationResult = {
+
+        containsValidationError : false,
+        validationSummary : ""
+
+    };
+
+    function clearProductCategory() {
+
+        $scope.productCategory.categoryName = "";
+        $scope.productCategory.categoryDetails = "";
+    }
+
+    function clearMessage() {
+
+        $scope.message.containsSuccessfulMessage = false;
+        $scope.message.successfulMessage = "";
+    }
+
+    function displayMessage() {
+
+        $scope.message.containsSuccessfulMessage = false;
+        $scope.message.successfulMessage = "";
+    }
+
     $scope.createProductCategory = function (productCategory) {
 
-        productCategoryService.createProductCategory(productCategory)
-        .success(function (data) {
+        var validationMessages = requiredFieldValidationService.validationErrorMessage(
+        [
+            { name: $scope.productCategory.categoryName || "", errorMessage: 'Enter name' },
+               { name: $scope.productCategory.categoryDetails || "", errorMessage: 'Enter details' }
 
-       /*     $timeout(function () {
-              
-             
-            }, 3000) */
+        ]);
 
-        });
+        if (validationMessages.length > 0) {
+
+            $scope.validationResult.containsValidationError = true;
+
+            angular.element("#validationErrorMessage").empty();
+            validationMessages.forEach(function (errorMessage) {
+
+                angular.element("<li></li>")
+                .html(errorMessage)
+                .appendTo("#validationErrorMessage")
+            });
+
+        }
+
+        else {
+
+            $scope.validationResult.containsValidationError = false;
+            productCategoryService.createProductCategory(productCategory)
+             .success(function (data) {
+
+                 if (data.status
+                     && data.status == 'successful')
+                     displayMessage;
+                 $timeout(function () {
+                     clearMessage();
+                     clearProductCategory();
+
+                 }, 5000)
+
+             });
+
+        }
 
     }
 
 }
+
---
+++ TutorialOnNodeJs/public/javascripts/app/requiredFieldValidationService.js
@@ -0,0 +1,33 @@
+﻿angular.module("productCategoryModule")
+.factory("requiredFieldValidationService", requiredFieldValidationService);
+
+
+function _getRequiredValidationMessage(requiredInfo) {
+
+    var errorMessage = [];
+    if (requiredInfo.length > 0) {
+
+        angular.forEach(requiredInfo, function (requiredInf) {
+
+            if (
+
+                requiredInfo.name !== 'undefined'
+
+                &&
+                  (
+                 requiredInfo.name == null
+                || requiredInfo.name == ''
+                || requiredInfo.name.length == 0)
+              ) {
+                errorMessage.push(requiredInfo.errorMessage);
+            }
+        });
+    }
+    return errorMessage;
+}
+return {
+
+    getRequiredValidationMessage: _getRequiredValidationMessage
+};
+}
+
